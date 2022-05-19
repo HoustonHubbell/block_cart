6204c4ceda2d650801f33e1b7d9b8284e101d32e
HoustonHubbell <hootie87229@gmail.com>
2022-05-18 20:09:17 -0500

    tim commit

---
+++ block_inventory/api/api.py
@@ -0,0 +1,44 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+
+class Crypto:
+
+    def get_top_5(self):
+
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+        parameters = {
+          'start':'1',
+          'limit':'5',
+          'convert':'USD'
+        }
+        headers = {
+          'Accepts': 'application/json',
+          'X-CMC_PRO_API_KEY': '99d6ba91-02aa-4c99-b566-7686a05f8eab',
+        }
+        session = Session()
+        session.headers.update(headers)
+
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        return data['data']
+
+    def get_top_10(self):
+
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+        parameters = {
+          'start':'1',
+          'limit':'10',
+          'convert':'USD'
+        }
+        headers = {
+          'Accepts': 'application/json',
+          'X-CMC_PRO_API_KEY': '99d6ba91-02aa-4c99-b566-7686a05f8eab',
+        }
+        session = Session()
+        session.headers.update(headers)
+
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        return data['data']
